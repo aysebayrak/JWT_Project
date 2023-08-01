87dded46a9c039e764a523b9fe786d63f8d01e68
AYŞE BAYRAK <ayse.bayrak812@gmail.com>
2023-08-01 14:42:10 +0300

    Add project files.

---
+++ JWT_Project/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using JWT_Project.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[Action]")]
+        public IActionResult Login()
+        {
+            return Created("",new BuildToken().CreateToken());
+        }
+
+        [Authorize]
+        [HttpGet("[Action]")]
+        public IActionResult CustomerList()
+        {
+            return Ok("müşteri litesine eriştiniz");
+        }
+    }
+}
